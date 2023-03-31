8fe2ce8a65b632a8d7b2049bb3fc9c3057c9b74e
drishtipeshwani <archiepeshwani09@gmail.com>
2023-03-31 13:19:07 +0530

    Initial changes

---
+++ public/script.js
@@ -0,0 +1,65 @@
+const socket = io('/');
+const peer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
+});
+
+const video = document.getElementById('videoPlayer');
+const fileInput = document.getElementById('fileInput');
+const sendButton = document.getElementById('sendButton');
+
+peer.on('open', function (id) {
+  // Unique peer join the dashboard
+  socket.emit('join', DASHBOARD_ID, id);
+  // Sender Side 
+  socket.on('userJoined', (userId) => {
+    const conn = peer.connect(userId.userId);
+    conn.on('open', function () {
+      sendButton.addEventListener('click', function () {
+        const file = fileInput.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsArrayBuffer(file);
+        fileReader.onload = function (event) {
+          const arrayBuffer = event.target.result;
+          conn.send(arrayBuffer)
+        }
+
+      });
+      conn.on('data', function (data) {
+        const blob = new Blob([data]);
+        const videoURL = window.URL.createObjectURL(blob);
+        video.src = videoURL;
+        video.play();
+      });
+    });
+  })
+  // Receiver Side 
+  socket.on('joined', () => {
+    peer.on('connection', (conn) => {
+      conn.on('open', function () {
+        sendButton.addEventListener('click', function () {
+          const file = fileInput.files[0];
+          const fileReader = new FileReader();
+          fileReader.readAsArrayBuffer(file);
+          fileReader.onload = function (event) {
+            const arrayBuffer = event.target.result;
+            conn.send(arrayBuffer)
+          }
+
+        });
+        conn.on('data', function (data) {
+          const blob = new Blob([data]);
+          const videoURL = window.URL.createObjectURL(blob);
+          video.src = videoURL;
+          video.play();
+        });
+      });
+    })
+  })
+});
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,36 @@
+// Creating a express server
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server); // Passing the server created to socket.io 
+const { v4: uuidV4 } = require('uuid')
+
+// Set up Express.js
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+});
+
+app.get('/:dashboard', (req, res) => {
+  res.render('dashboard', { dashboardId: req.params.dashboard })
+})
+
+// Set up Socket.io
+io.on('connection', socket => {
+  // Server starts listening for connections
+  // Maintaining a dictionary for mapping userId for a particular dashboardId
+  socket.on('join', (dashboardId, userId) => {
+    socket.join(dashboardId)
+    // Tell that another peer has joined
+    socket.emit('joined')
+    socket.broadcast.emit('userJoined',{userId:userId})
+  })
+})
+
+// Start the server
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
